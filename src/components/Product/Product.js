8cabff449a3232c33f1c6c497839f80c7c9674de
doron.nahari@sap.com <doron.nahari@sap.com>
2022-01-31 17:04:56 +0200

    init

---
+++ src/components/Product/Product.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Product.css';
+import {Card} from "antd";
+import Meta from "antd/es/card/Meta";
+import {AppstoreAddOutlined} from "@ant-design/icons";
+
+const Product = (props) => {
+    const handleClick = () => {
+        props.click(props.product)
+    }
+    return (
+        <Card
+            style={{background: "white"}}
+            cover={
+                <img style={{height: "150px"}} src={props.product.image} alt="empty"/>
+            }
+            actions={[<div onClick={handleClick}>{props.isCartView ? "Remove from Cart" : "Add to Cart" }</div>]}
+        >
+            <Meta title={props.product.name}/>
+        </Card>
+    )
+};
+
+Product.propTypes = {
+    product: PropTypes.object,
+    click: PropTypes.func,
+    isCartView: PropTypes.bool
+};
+
+export default Product;
